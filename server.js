ee05e6c6dc758081e640f5c64ddef707ad8e6092
Subhash Negi <negisubhash25@gmail.com>
2021-01-17 10:25:11 +0530

    heroku app hosting

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const port = process.env.port || 3000
+
+
+
+http.listen(port, () => {
+    console.log("listening");
+})
+
+//app.use(express.static("C:/Users/DELL/Desktop/practice/projects/chatApp/public"));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+    //res.sendFile("C:/Users/DELL/Desktop/practice/projects/chatApp/index.html");
+})
+
+
+
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('connected...');
+    socket.on('message', (msg) => {
+       socket.broadcast.emit('message', msg)
+    })
+})
